e2751ecdb18569a7af2a22fa6225bff781338e3c
wermvasconcelos <wermenson.vasconcelo@gmail.com>
2024-11-25 17:00:18 -0300

    feat: search pokemon

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,26 +2,61 @@
   <v-app>
     <v-container>
       <h1>Pokédex - Vuetify</h1>
-      <br>
-      <Loading v-if="initialLoading"/>
+      <br />
+      <Loading v-if="initialLoading" />
+      <!-- Campo de busca -->
+      <v-text-field 
+        v-if="!initialLoading" 
+        v-model="searchPokemon" 
+        label="Buscar Pokémon" 
+        class="mb-4"
+      ></v-text-field>
+      
       <v-row>
-        <!-- Itera sobre a lista de Pokémon e exibe cada um -->
-        <v-col v-for="pokemon in pokemons" :key="pokemon.name" cols="12" sm="6" md="3">
+        <!-- Itera apenas sobre os Pokémon filtrados -->
+        <v-col 
+          v-for="pokemon in filteredPokemons" 
+          :key="pokemon.name" 
+          cols="12" 
+          sm="6" 
+          md="3"
+        >
           <v-card @click="openDialog(pokemon)">
             <v-card-title class="d-flex justify-space-between align-center">
               <span>{{ pokemon.name }}</span>
               <div>
-                <v-chip v-for="(type, index) in pokemon.types" :key="index" :color="getTypeClass(type)" class="ma-1"
-                  outlined>
+                <v-chip 
+                  v-for="(type, index) in pokemon.types" 
+                  :key="index" 
+                  :color="getTypeClass(type)" 
+                  class="ma-1"
+                  outlined
+                >
                   {{ type }}
                 </v-chip>
               </div>
             </v-card-title>
             <v-card-subtitle>Nº {{ pokemon.id }}</v-card-subtitle>
-            <v-img :src="pokemon.sprites.front_default" alt="{{ pokemon.name }}" height="150" />
+            <v-img 
+              :src="pokemon.sprites.front_default" 
+              alt="{{ pokemon.name }}" 
+              height="150" 
+            />
           </v-card>
         </v-col>
       </v-row>
+
+      <!-- Botão para mostrar mais -->
+      <v-container class="d-flex justify-center align-center">
+        <v-btn 
+          v-if="!initialLoading && pokemons.length > visiblePokemons.length" 
+          @click="showMorePokemons"
+          color="primary" 
+          class="mt-4"
+        >
+          Mostrar mais
+        </v-btn>
+      </v-container>
 
       <!-- Modal de Detalhes do Pokémon -->
       <v-dialog v-model="dialog" max-width="500px">
@@ -33,8 +68,11 @@
             Nº {{ selectedPokemon?.id }}
           </v-card-subtitle>
           <v-card-text>
-            <v-img :src="selectedPokemon?.sprites_gif.front_default" alt="Imagem do Pokémon" height="200"></v-img>
-            <!-- Aqui você pode adicionar mais informações sobre o Pokémon -->
+            <v-img 
+              :src="selectedPokemon?.sprites_gif.front_default" 
+              alt="Imagem do Pokémon" 
+              height="200"
+            ></v-img>
           </v-card-text>
           <v-card-actions>
             <v-btn color="primary" @click="dialog = false">Fechar</v-btn>
@@ -46,7 +84,7 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import axios from 'axios';
 
 import Loading from '@/components/Loading.vue';
@@ -60,42 +98,35 @@ interface Pokemon {
   sprites_gif: {
     front_default: string;
   };
-  types: string[]; // Array de strings com os tipos do Pokémon
+  types: string[];
 }
 
-// Array reativo para armazenar os Pokémon
+const searchPokemon = ref('');
 const pokemons = ref<Pokemon[]>([]);
-
-// Estado para configurar "loading"
+const visiblePokemons = ref<Pokemon[]>([]);
 const initialLoading = ref(true);
-
-// Estado para controlar a exibição do modal
 const dialog = ref(false);
 const selectedPokemon = ref<Pokemon | null>(null);
 
-// Função para buscar os primeiros 151 Pokémon
 const fetchPokemons = async () => {
   initialLoading.value = true;
 
   try {
-    // Cria um array de promessas para os IDs de 1 a 151
     const requests = Array.from({ length: 1025 }, (_, i) =>
       axios.get(`https://pokeapi.co/api/v2/pokemon/${i + 1}`)
     );
 
-    // Aguarda a conclusão de todas as requisições
     const responses = await Promise.all(requests);
 
-    // Extrai os dados relevantes de cada resposta
     pokemons.value = responses.map((response) => ({
       name: response.data.name,
       id: response.data.id,
       sprites: response.data.sprites,
       sprites_gif: response.data.sprites.other.showdown,
-      types: response.data.types.map((type: { type: { name: string } }) => type.type.name), // Extraindo o nome de cada tipo
+      types: response.data.types.map((type: { type: { name: string } }) => type.type.name),
     }));
 
-    console.log(pokemons.value);
+    visiblePokemons.value = pokemons.value.slice(0, 20);
   } catch (error) {
     console.error('Erro ao buscar os Pokémon:', error);
   }
@@ -103,13 +134,16 @@ const fetchPokemons = async () => {
   initialLoading.value = false;
 };
 
-// Função para abrir o modal com as informações do Pokémon selecionado
+const showMorePokemons = () => {
+  const currentLength = visiblePokemons.value.length;
+  visiblePokemons.value = pokemons.value.slice(0, currentLength + 10);
+};
+
 const openDialog = (pokemon: Pokemon) => {
   selectedPokemon.value = pokemon;
   dialog.value = true;
 };
 
-// Função para retornar a classe de cor do tipo
 const getTypeClass = (type: string) => {
   const typeColors: { [key: string]: string } = {
     grass: 'green-accent-4',
@@ -131,8 +165,17 @@ const getTypeClass = (type: string) => {
     fairy: 'pink-accent-3',
   };
 
-  return typeColors[type] || 'grey lighten-2'; // Padrão para tipos não encontrados
+  return typeColors[type] || 'grey lighten-2';
 };
+
+// Computed para retornar a lista filtrada de Pokémon
+const filteredPokemons = computed(() => {
+  if (!searchPokemon.value) return visiblePokemons.value;
+
+  return pokemons.value.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchPokemon.value.toLowerCase())
+  );
+});
 
 // Chama a função ao montar o componente
 fetchPokemons();
