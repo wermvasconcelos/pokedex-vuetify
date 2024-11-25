6dc28d1773ee2a3fe293916743a284766393567c
wermvasconcelos <wermenson.vasconcelo@gmail.com>
2024-11-25 10:08:51 -0300

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <HelloWorld /> -->
+</template>
+
+<script lang="ts" setup>
+  //
+  import axios from 'axios';
+
+  axios({
+    method: 'get',
+    url: 'https://pokeapi.co/api/v2/pokemon/ditto',
+    responseType: 'json',
+  }).then((response) => {
+    console.log(response.data)
+  })
+</script>
