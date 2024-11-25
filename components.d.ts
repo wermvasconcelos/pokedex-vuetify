a08dffea29fa4f399ce1df9ce2f262366bed90c0
wermvasconcelos <wermenson.vasconcelo@gmail.com>
2024-11-25 16:13:56 -0300

    feat: loading

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    Loading: typeof import('./src/components/Loading.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
